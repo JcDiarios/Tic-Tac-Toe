cc274c4721f15a7685d7e4736f01e382c3561aa2
JcDiarios <jcarlodiarios@gmail.com>
2023-11-10 22:49:12 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tictactoe from './components/TicTacToe/Tictactoe.jsx'
+
+function App() {
+  return (
+    <div>
+      <Tictactoe />
+    </div>
+  )
+}
+
+export default App
